1c30d4537fc69c7334525b7ff003b21445fef216
Daniele Pasqui <daniele.pasqui@softecspa.it>
2017-07-17 12:11:18 +0200

    eliminato uid dalla chiamata

--- www/js/core/manager/QueryManager.js
+++ www/js/core/manager/QueryManager.js
@@ -3,13 +3,13 @@
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU Affero General Public License
    as published by the Free Software Foundation.
-   The interactive user interfaces in modified source and object code versions 
-   of this program must display Appropriate Legal Notices, as required under 
-   Section 5 of the GNU Affero GPL . In accordance with Section 7(b) of the 
-   GNU Affero GPL , these Appropriate Legal Notices must retain the display 
+   The interactive user interfaces in modified source and object code versions
+   of this program must display Appropriate Legal Notices, as required under
+   Section 5 of the GNU Affero GPL . In accordance with Section 7(b) of the
+   GNU Affero GPL , these Appropriate Legal Notices must retain the display
    of the "Sii-Mobility Dev Kit Mobile App Km4City" logo. The Logo "Sii-Mobility
   Dev Kit Mobile App Km4City" must be a clickable link that leads directly to the
-  Internet URL http://www.sii-mobility.org oppure a DISIT Lab., using 
+  Internet URL http://www.sii-mobility.org oppure a DISIT Lab., using
   technology derived from  Http://www.km4city.org.
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,7 @@
    GNU General Public License for more details.
    You should have received a copy of the GNU Affero General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 var QueryManager = {
 
@@ -39,7 +39,7 @@ var QueryManager = {
     format: "json",
 
     refreshParameters: function() {
-	
+
 		if (SettingsManager.maxDistanceRecommender != null) {
             QueryManager.maxDistsRecommender = SettingsManager.maxDistanceRecommender;
         } else {
@@ -95,11 +95,11 @@ var QueryManager = {
         if (QueryManager.maxDists === null || QueryManager.maxResults === null) {
             QueryManager.refreshParameters();
         }
-        return "?selection=" + queryCoordinates.join(";") + "&requestFrom=" + requestFrom + "&categories=" + categories.join(";") + "&maxResults=" + QueryManager.maxResults + "&maxDists=" + QueryManager.maxDists + "&format=" + QueryManager.format + "&uid=" + QueryManager.uid + "&lang=" + QueryManager.language + "&geometry=true";
+        return "?selection=" + queryCoordinates.join(";") + "&requestFrom=" + requestFrom + "&categories=" + categories.join(";") + "&maxResults=" + QueryManager.maxResults + "&maxDists=" + QueryManager.maxDists + "&format=" + QueryManager.format + "&lang=" + QueryManager.language + "&geometry=true";
     },
 
     createServiceQuery: function (serviceUri, requestFrom) {
-        return "?serviceUri=" + serviceUri + "&requestFrom=" + requestFrom + "&format=" + QueryManager.format + "&uid=" + QueryManager.uid + "&lang=" + QueryManager.language;
+        return "?serviceUri=" + serviceUri + "&requestFrom=" + requestFrom + "&format=" + QueryManager.format + "&lang=" + QueryManager.language;
     },
 
     createTextQuery: function (text, queryCoordinates, requestFrom) {
@@ -121,7 +121,7 @@ var QueryManager = {
     createLastFeedbackQuery: function (requestFrom) {
         return "feedback/last/?lang=" + QueryManager.language + "&maxResults=" + 8 + "&uid=" + QueryManager.uid + "&requestFrom=" + requestFrom;
     },
-    
+
     createEventsQuery: function(time, requestFrom){
         return "events/?range=" + time + "&requestFrom=" + requestFrom + "&uid=" + QueryManager.uid;
     },
@@ -138,7 +138,7 @@ var QueryManager = {
         LogRecommender.write(dateFormat(new Date(), "[yyyy-mm-dd HH:MM:ss]") + "getSuggestions " + " latitude=" + queryCoordinates[0] + " longitude=" + queryCoordinates[1] + " mode=" + mode + " distance=" + QueryManager.maxDistsRecommender);
         return "?action=recommend&uid=" + QueryManager.uid + "&uid2=" + QueryManager.uid2 + "&appID=" + QueryManager.appID + "&profile=" + QueryManager.profile + "&version=" + QueryManager.version + "&latitude=" + queryCoordinates[0] + "&longitude=" + queryCoordinates[1] + "&mode=" + mode + "&aroundme=" + aroundMe + "&distance=" + QueryManager.maxDistsRecommender + "&lang=" + QueryManager.language;
     },
-    
+
     createRecommendAGroupQuery: function(queryCoordinates, groupToRecommend, mode) {
 		LogRecommender.write(dateFormat(new Date(), "[yyyy-mm-dd HH:MM:ss]") + "getGroupSuggestions " + " group=" + groupToRecommend + " latitude=" + queryCoordinates[0] + " longitude=" + queryCoordinates[1] + " mode=" + mode + " distance=" + QueryManager.maxDistsRecommender);
 		return "?action=recommendForGroup&uid=" + QueryManager.uid + "&uid2=" + QueryManager.uid2 + "&appID=" + QueryManager.appID + "&profile=" + QueryManager.profile + "&version=" + QueryManager.version + "&latitude=" + queryCoordinates[0] + "&longitude=" + queryCoordinates[1] + "&mode=" + mode + "&group=" + groupToRecommend + "&distance=" + QueryManager.maxDistsRecommender + "&lang=" + QueryManager.language;
@@ -148,7 +148,7 @@ var QueryManager = {
     	LogRecommender.write(dateFormat(new Date(), "[yyyy-mm-dd HH:MM:ss]") + "dislikeGroup " + " group=" + groupToDislike);
     	return "?action=dislike&uid=" + QueryManager.uid + "&group=" + groupToDislike + "&lang=" + QueryManager.language;
     },
-    
+
     createDislikeSubCategoryQuery: function(subCategoryToDislike) {
     	LogRecommender.write(dateFormat(new Date(), "[yyyy-mm-dd HH:MM:ss]") + "dislikeSubCategory " + " subcategory=" + subCategoryToDislike);
     	return "?action=dislikeSubclass&uid=" + QueryManager.uid + "&subclass=" + subCategoryToDislike + "&lang=" + QueryManager.language;
@@ -167,7 +167,7 @@ var QueryManager = {
     createLogPrincipalMenuChoices: function (buttonId, requestFrom) {
         return "notification/?uid=" + QueryManager.uid + "&selection=" + buttonId + "&requestFrom=" + requestFrom;
     },
-    
+
     createLogRemoveMessagePersonalAssistant: function(messageId){
         return "cancel-engagement?id=" + messageId;
     },
